ab9388f0202e78a38acd1cc1cbcfdb0d31ed6b0d
kamilbaranek <kamil.baranek@me.com>
2025-11-18 13:45:27 +0100

    chore: fix worker build and pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  apps: [
+    {
+      name: 'seobooster-api',
+      cwd: __dirname,
+      script: 'npm',
+      args: 'run start:api',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      max_memory_restart: '512M'
+    },
+    {
+      name: 'seobooster-web',
+      cwd: __dirname,
+      script: 'npm',
+      args: 'run start:web',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      max_memory_restart: '512M'
+    },
+    {
+      name: 'seobooster-worker',
+      cwd: __dirname,
+      script: 'npm',
+      args: 'run start:worker',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      max_memory_restart: '512M'
+    }
+  ]
+};
+
